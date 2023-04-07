0cdb7263d05daf0380c3ef5e7b2167121de53082
Tengau <tzhuoga@u.rochester.edu>
2023-04-06 21:17:05 -0400

    converted ultrasound code to be ros compatible

--- src/perception/src/b2a.py
+++ src/perception/src/b2a.py
@@ -1,19 +1,41 @@
 import serial
-
-# Specify the serial port and baud rate
-serial_port = '/dev/ttyACM0'
-baud_rate = 9600
-
-# Open the serial port
-ser = serial.Serial(serial_port, baud_rate)
-
-while True:
-    # Read the distance measurement from the Arduino
-    distance = ser.readline().strip()
+import rospy
+from perception.msg import FloatArray
     
-    # Print the distance measurement
-    print("Object is ",distance," cm away.")
+def main():
+    # Specify the serial port and baud rate
+    serial_port = '/dev/ttyACM0'
+    baud_rate = 9600
 
+    # Open the serial port
+    ser = serial.Serial(serial_port, baud_rate)
+    print("ultrasounds are connected...")
+
+    # num_us = 1
+    us_pub = rospy.Publisher('ultrasound_publisher', FloatArray)
+    rospy.init_node('ultrasound_node', anonymous=True)
+    r = rospy.Rate(0.2)
+
+    while not rospy.is_shutdown():
+        print("ultrasound loop is running...")
+        # Read the distance measurement from the Arduino
+        distance = ser.readline().strip()
+        
+        # Print the distance measurement
+        print("Object is ",distance," cm away.")
+
+        # for future array implementation:
+        #for i in range(num_us):
+        #    arr[i] = distance
         
 
+        msg = FloatArray()
+        msg.data = [distance]
 
+        us_pub.publish(msg)
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException: pass
