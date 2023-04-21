597730ea36201f4db330425a742172b5d0ed4614
Tengau <tzhuoga@u.rochester.edu>
2023-04-20 20:53:25 -0400

    ran tests on virtual machine

--- src/perception/src/perception_node_simple.py
+++ src/perception/src/perception_node_simple.py
@@ -3,8 +3,7 @@ from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 import math
 
-
-pub = rospy.Publisher('obstacle', Bool, queue_size = 1)
+pub = rospy.Publisher('obstacle', Bool, queue_size = 10)
 obstacle = Bool()
 count = 0
 
@@ -15,13 +14,11 @@ def callback(data):
     # obstacle = Bool()
     # NDArray of angles and distances are collected:i
     for i in range(int((data.angle_max - data.angle_min) / data.angle_increment)):
-        
         # print("current angle: ", i, ": dist: ", data.ranges[i])
-        
         if(data.ranges[i] <= 1.5 and data.ranges[i] >= data.range_min): # if valid point and checks before 2 meters
         #if(data.ranges[i] <= data.range_max and data.ranges[i] >= data.range_min): # if valid point
-
-            if((60 > i) and (i > 120)):  # if obstacle in front
+            print("angle:", i)
+            if((60 < i) and (i < 120)):  # if obstacle in front
                 obstacle.data = True
                 print("obstacle detected, stop command sent")
                 count = count + 1
@@ -29,7 +26,6 @@ def callback(data):
             else:
                 obstacle.data = False
                 #print("path clear so far")
-
     #pub.publish(obstacle)
     if (obstacle.data == False):
         print("path clear so far-------------------")
@@ -38,12 +34,10 @@ def callback(data):
     count = count + 1
 
 def main():
-    global pub, obstacle
-
     rospy.init_node('perception_node', anonymous=True)
     rospy.Subscriber("scan", LaserScan, callback)
-    rate = rospy.Rate(1) # every second
-    #rospy.sleep(40)
+    rate = rospy.Rate(2) # every second
+    rospy.sleep(10)
     while not rospy.is_shutdown():
         pub.publish(obstacle)
         print(count)
